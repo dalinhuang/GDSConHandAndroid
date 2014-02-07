22d792874e81985adced924349932d26e6ed4653
haleyshi <sgh1982@gmail.com>
2014-02-07 17:10:21 +0800

    Change VERSION_NAME to PUBLIC since we need the start page to prepare for everything ready, e.g. network configuration, map files...

--- GDSConHand/src/com/ericsson/cgc/aurora/wifiindoor/util/SoftwareVersionData.java
+++ GDSConHand/src/com/ericsson/cgc/aurora/wifiindoor/util/SoftwareVersionData.java
@@ -1,5 +1,5 @@
 package com.ericsson.cgc.aurora.wifiindoor.util;
 
 public class SoftwareVersionData {
-	public static String VERSION_NAME = "GDSC";
+	public static String VERSION_NAME = "PUBLIC";
 }
