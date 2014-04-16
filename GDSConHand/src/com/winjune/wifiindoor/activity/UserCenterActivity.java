5534eb312f5209a17f86fdce9d63c365c779d534
jeffzha <726144@qq.com>
2014-04-16 15:43:44 +0800

    UI for version check and upgrade #68

--- GDSConHand/src/com/winjune/wifiindoor/activity/UserCenterActivity.java
+++ GDSConHand/src/com/winjune/wifiindoor/activity/UserCenterActivity.java
@@ -1,9 +1,14 @@
 package com.winjune.wifiindoor.activity;
 
 import com.winjune.wifiindoor.R;
+import com.winjune.wifiindoor.util.IndoorMapData;
+import com.winjune.wifiindoor.util.Util;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.view.View;
 
@@ -38,7 +43,42 @@ public class UserCenterActivity extends Activity {
 	}
 	
 	public void checkUpdateBarClick(View v){
-		
+		StringBuffer sb = new StringBuffer();  
+		sb.append(this.getString(R.string.app_name));
+		sb.append("V3.1版,更新说明:");
+	    sb.append("\r\n" + "1. 解决部分情况下卡顿或重启问题；");  
+	    sb.append("\r\n" + "2. 解决Android4.4系统下某些情况下无法识别存储卡问题；");
+	    sb.append("\r\n" + "3. 性能优化。");
+	    
+	    AlertDialog.Builder builder = new AlertDialog.Builder(this);	
+		builder.setTitle("版本更新提示");	
+		builder.setMessage(sb.toString());
+	    builder.setNegativeButton(R.string.download_later,  
+            new DialogInterface.OnClickListener() {  
+                public void onClick(DialogInterface dialog,  
+                        int whichButton) { 
+                    //finish();  
+                }  
+            });
+	   
+		builder.setPositiveButton(R.string.download_now,
+	            new OnClickListener() {  
+	                @Override  
+	                public void onClick(DialogInterface dialog,  
+	                        int which) { 
+	                    Util.downFile(UserCenterActivity.this, 
+	                    		Util.fullUrl(IndoorMapData.APK_FILE_PATH_REMOTE, "/"),
+	                    		IndoorMapData.APK_FILE_PATH_LOCAL,
+	                    		"WifiIPS_"+"GDSC"+".apk",
+	                    		true,             // Open after download
+	                    		"application/vnd.android.package-archive",
+	                    		true,           // Use Handler
+	                    		true); 			// Use Thread
+	                }  
+	            });
+		    
+	    builder.create();
+	    builder.show();
 	}
 	
 	public void aboutBarClick(View v){
