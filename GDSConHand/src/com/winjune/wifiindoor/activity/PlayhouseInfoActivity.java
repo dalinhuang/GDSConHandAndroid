e5caad4c9b87884622a589de4c3b8f08ae463d3e
derekhuang <derek.huang@winjune.com>
2014-04-17 22:34:40 +0800

    Let the user choose the alarm time

--- GDSConHand/src/com/winjune/wifiindoor/activity/PlayhouseInfoActivity.java
+++ GDSConHand/src/com/winjune/wifiindoor/activity/PlayhouseInfoActivity.java
@@ -11,10 +11,13 @@ import com.winjune.wifiindoor.util.Util;
 import android.os.Bundle;
 import android.app.Activity;
 import android.app.AlarmManager;
+import android.app.AlertDialog;
 import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.View;
@@ -29,6 +32,7 @@ import android.widget.Toast;
 public class PlayhouseInfoActivity extends Activity {
 
 	private AlarmManager mAlarmMgr;
+	private int mMinutesAhead;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -53,6 +57,8 @@ public class PlayhouseInfoActivity extends Activity {
 		});
 		
 		mAlarmMgr = (AlarmManager) getSystemService(Service.ALARM_SERVICE);
+		
+		mMinutesAhead = 5; //By default the alarm time is 5 minutes ahead
 	}
 	
 	public class PlayhouseTimeList extends ArrayAdapter<DummyContent.DummyItem> {
@@ -84,33 +90,85 @@ public class PlayhouseInfoActivity extends Activity {
 	
 	public void onSetAlarmClick(View v){
 		
-		Calendar currentTime = Calendar.getInstance();
-		
-		int startHour = 16; // TBD: this should be got from the adpater or the textView
-		int startMinute = 56; // TBD: this should be got from the adpater or the textView
-		int minutesAhead = 1; //TBD: this should be chosen by the user
-		
-		if (currentTime.get(Calendar.HOUR_OF_DAY) > startHour){
-			Util.showToast(PlayhouseInfoActivity.this, "剧场已开始！", Toast.LENGTH_SHORT);
-			return;
-		}
-		
-		Intent intent = new Intent(PlayhouseInfoActivity.this, AlarmActivity.class);
-		PendingIntent pi = PendingIntent.getActivity(PlayhouseInfoActivity.this, 0, intent, 0);
-		
-		if (startMinute < minutesAhead){
-			startHour = startHour - 1;
-			startMinute = startMinute + 60 - minutesAhead;
-		}
-		else{
-			startMinute = startMinute - minutesAhead;
-		}
-		
-		currentTime.set(Calendar.HOUR_OF_DAY, startHour);
-		currentTime.set(Calendar.MINUTE, startMinute);
-		
-		mAlarmMgr.set(AlarmManager.RTC, currentTime.getTimeInMillis(), pi);
-		
-		Util.showToast(PlayhouseInfoActivity.this, "设置提醒成功！", Toast.LENGTH_SHORT);
+		AlertDialog.Builder builder = new AlertDialog.Builder(PlayhouseInfoActivity.this);
+		builder.setTitle("请选择提前提醒的时间")
+		.setSingleChoiceItems(R.array.alarm_time, 0, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				switch (which){
+					case 0:
+						mMinutesAhead = 5;
+						break;
+					case 1:
+						mMinutesAhead = 10;
+						break;
+					case 2:
+						mMinutesAhead = 15;
+						break;
+					case 3:
+						mMinutesAhead = 30;
+						break;
+					case 4:
+						mMinutesAhead = 60;
+						break;
+					case 5:
+						mMinutesAhead = 1;
+						break;
+					default:
+						break;
+				}
+			}
+		})
+		.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				int startHour = 22; // TBD: this should be got from the adpater or the textView
+				int startMinute = 33; // TBD: this should be got from the adpater or the textView
+				
+				Calendar currentTime = Calendar.getInstance();
+				
+				if (currentTime.get(Calendar.HOUR_OF_DAY) > startHour){
+					Util.showToast(PlayhouseInfoActivity.this, "剧场已开始！", Toast.LENGTH_SHORT);
+					return;
+				}
+				
+				Intent intent = new Intent(PlayhouseInfoActivity.this, AlarmActivity.class);
+				PendingIntent pi = PendingIntent.getActivity(PlayhouseInfoActivity.this, 0, intent, 0);
+				
+				if (startMinute < mMinutesAhead){
+					startHour = startHour - 1;
+					startMinute = startMinute + 60 - mMinutesAhead;
+				}
+				else{
+					startMinute = startMinute - mMinutesAhead;
+				}
+				
+				currentTime.set(Calendar.HOUR_OF_DAY, startHour);
+				currentTime.set(Calendar.MINUTE, startMinute);
+				
+				mAlarmMgr.set(AlarmManager.RTC, currentTime.getTimeInMillis(), pi);
+				
+				TextView tv = (TextView) findViewById(R.id.schedule_text_remind);
+				tv.setText(R.string.alarm_set);
+				tv.setTextColor(Color.RED);
+				
+				Util.showToast(PlayhouseInfoActivity.this, "设置提醒成功！", Toast.LENGTH_SHORT);
+
+				dialog.dismiss();
+			}
+		})
+		.setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+			
+			@Override
+			public void onClick(DialogInterface dialog, int which) {
+				// TODO Auto-generated method stub
+				dialog.dismiss();
+			}
+		})
+		.show();
 	}
 }
